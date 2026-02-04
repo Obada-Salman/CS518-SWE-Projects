81544360a4616c6c23f7dfd002a7a836d6eeec25
Obada Salman <salmanos@sunypoly.edu>
2026-02-04 09:49:19 -0500

    gravity and jump mechanic

--- obada/main.py
+++ obada/main.py
@@ -7,6 +7,10 @@ resHeight = 600
 screen = pygame.display.set_mode((resWidth, resHeight))
 
 FPS = 120
+gravity = 0.3
+jump_strength = 12
+player_vy = 0.0
+npc_vy = 0.0
 clock = pygame.time.Clock()
 
 # Load player
@@ -14,6 +18,7 @@ player_image = pygame.image.load('obada/player.png')
 h, w = player_image.get_size()
 player_sprite = pygame.transform.scale(player_image, (w // 16, h // 16))
 player = player_sprite.get_rect(center=(400, 300))
+player_y = float(player.y)
 player_health = 100
 
 # Load carrot NPC
@@ -21,6 +26,7 @@ npc_image = pygame.image.load('obada/carrot.png')
 h, w = npc_image.get_size()
 npc_sprite = pygame.transform.scale(npc_image, (w // 4, h // 4))
 npc = npc_sprite.get_rect(center=(600, 300))
+npc_y = float(npc.y)
 npc_health = 85
 
 # Load map
@@ -28,6 +34,21 @@ map_image = pygame.image.load('obada/map.jpg')
 
 running = True
 while running:
+    player_vy += gravity
+    player_y += player_vy
+    npc_vy += gravity
+    npc_y += npc_vy
+
+    if player_y + player.height >= resHeight:
+        player_y = resHeight - player.height
+        player_vy = 0.0
+    player.y = int(player_y)
+
+    if npc_y + npc.height >= resHeight:
+        npc_y = resHeight - npc.height
+        npc_vy = 0.0
+    npc.y = int(npc_y)
+
     screen.blit(map_image, (0, 0))
 
     if player_health <= 0:
@@ -59,13 +80,13 @@ while running:
     # Player movement
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_RIGHT]:
-        player.move_ip(1, 0)
-    elif key[pygame.K_UP]:
-        player.move_ip(0, -1)
-    elif key[pygame.K_DOWN]:
-        player.move_ip(0, 1)
+        player.move_ip(-3, 0)
+    if key[pygame.K_RIGHT]:
+        player.move_ip(3, 0)
+    if key[pygame.K_UP] and player.bottom >= resHeight:
+        player_vy = -jump_strength
+    if key[pygame.K_DOWN]:
+        player.move_ip(0, 3)
 
     # Collision with screen borders
     if player.left < 0:
@@ -77,18 +98,31 @@ while running:
     if player.bottom > resHeight:
         player.bottom = resHeight
 
+    player_y = float(player.y)
+
+    if npc.left < 0:
+        npc.left = 0
+    if npc.right > resWidth:
+        npc.right = resWidth
+    if npc.top < 0:
+        npc.top = 0
+    if npc.bottom > resHeight:
+        npc.bottom = resHeight
+
+    npc_y = float(npc.y)
+
     # Collision with carrot
     if player.colliderect(npc):
         if key[pygame.K_LEFT]:
-            player.move_ip(1, 0)
+            player.move_ip(3, 0)
         elif key[pygame.K_RIGHT]:
-            player.move_ip(-1, 0)
+            player.move_ip(-3, 0)
         elif key[pygame.K_UP]:
-            player.move_ip(0, 1)
+            player.move_ip(0, 3)
         elif key[pygame.K_DOWN]:
-            player.move_ip(0, -1)
+            player.move_ip(0, -3)
         
-        # Decrease health when colliding
+        player_y = float(player.y)
         player_health -= 1
         npc_health -= 1
 
