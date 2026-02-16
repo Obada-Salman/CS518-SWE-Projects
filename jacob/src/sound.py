76f3e79b8af10541b5eddada00cc3355b9d46c85
Jacob Cormier <cormiej@sunypoly.edu>
2026-02-16 09:29:41 -0500

    clean up for new branch

--- jacob/src/sound.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.mixer.init()
-background_music = pygame.mixer.Sound("garden_background.mp3")
-background_music.set_volume(.5)
-click_sound = pygame.mixer.Sound("mouse_click.mp3")
-click_sound.set_volume(.75)
--- sawyer/vegetable_war/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install dependencies
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-# Run the game
-echo "Starting Onions May Cry..."
-cd src
-python main.py
